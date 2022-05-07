c648f7a4f0334f9401b011b06a1d19eb8835cf7a
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-05-07 14:35:14 +0900

    putExtraで値渡しサンプル

--- app/src/main/java/com/example/transitionsample/SecondActivity.kt
+++ app/src/main/java/com/example/transitionsample/SecondActivity.kt
@@ -2,6 +2,7 @@ package com.example.transitionsample
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
@@ -10,6 +11,12 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
 
         val btnBack: Button = findViewById(R.id.btnBack)
+        val tv: TextView = findViewById(R.id.tv)
+
+        // 渡された値を取り出す⇒テキストに表示
+        val text = intent.getStringExtra("TEXT")
+        tv.text = text + "さん"
+
         btnBack.setOnClickListener {
             finish()
         }
